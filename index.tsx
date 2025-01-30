27d05f9dacf97acc5c468728daa168619c2f7972
Benjamin Dobson <35073256+benj-dobs@users.noreply.github.com>
2025-01-30 16:52:49 +0000

    Simplify repro

--- index.tsx
+++ index.tsx
@@ -1,8 +1,5 @@
-import { FC, type PropsWithChildren } from "react";
-import { chakra, type SystemStyleObject } from "@chakra-ui/react";
+import { type SystemStyleObject } from "@chakra-ui/react";
 
 declare const hoverStyles: SystemStyleObject;
 
-export const Component: FC<PropsWithChildren> = (props) => {
-  return <chakra.div __css={{ "&:hover": { ...hoverStyles } }} {...props} />;
-};
+export const testObject = { ...hoverStyles };
